74fb299326ac560fd621e0e89ba7b41703742faa
aasifbkhan <aasifbkhan98@gmail.com>
2023-07-05 20:22:41 +0530

    book list implementation with ngrx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GoogleBooksService } from './components/book-list/state/books.service';
+import { Store } from '@ngrx/store';
+import { BooksActions, BooksApiActions } from './components/book-list/state/books.actions';
+import { selectBookCollection, selectBooks } from './components/book-list/state/books.selectors';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'abc-app';
+
+  books$ = this.store.select(selectBooks);
+  bookCollection$ = this.store.select(selectBookCollection);
+
+  constructor(private booksService: GoogleBooksService, private store: Store) { }
+
+  ngOnInit() {
+    this.booksService
+      .getBooks()
+      .subscribe((books) =>
+        this.store.dispatch(BooksApiActions.retrievedBookList({ books }))
+      );
+  }
+
+  onAdd(bookId: string) {
+    this.store.dispatch(BooksActions.addBook({ bookId }));
+  }
+
+  onRemove(bookId: string) {
+    this.store.dispatch(BooksActions.removeBook({ bookId }));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,11 @@ import { CounterButtonsComponent } from './components/counter/counter-buttons/co
 import { counterReducer } from './components/counter/state/counter.reducer';
 import { CustomCounterInputComponent } from './components/counter/custom-counter-input/custom-counter-input.component';
 import { FormsModule } from '@angular/forms';
+import { booksReducer } from './components/book-list/state/books.reducer';
+import { collectionReducer } from './components/book-list/state/collection.reducer';
+import { BookListComponent } from './components/book-list/book-list/book-list.component';
+import { BookCollectionComponent } from './components/book-list/book-collection/book-collection.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,12 +23,15 @@ import { FormsModule } from '@angular/forms';
     CounterOutputComponent,
     CounterButtonsComponent,
     CustomCounterInputComponent,
+    BookListComponent,
+    BookCollectionComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule,
-    StoreModule.forRoot({ counter: counterReducer })
+    StoreModule.forRoot({ counter: counterReducer, books: booksReducer, collection: collectionReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
